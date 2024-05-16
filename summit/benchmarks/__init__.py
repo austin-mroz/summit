5aefc46c83505292407ff1e263080a16974429b8
austin-mroz <austin.mroz@gmail.com>
2024-05-16 23:57:04 +0100

    remove snar

--- summit/benchmarks/__init__.py
+++ summit/benchmarks/__init__.py
@@ -1,4 +1,4 @@
-from .snar import SnarBenchmark
-from .test_functions import Himmelblau, Hartmann3D, ThreeHumpCamel, DTLZ2, VLMOP2
 from .experimental_emulator import *
 from .MIT import *
+from .test_functions import (DTLZ2, VLMOP2, Hartmann3D, Himmelblau,
+                             ThreeHumpCamel)
